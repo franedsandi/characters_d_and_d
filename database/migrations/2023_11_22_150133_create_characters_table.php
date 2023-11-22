d4006f30a1c13afd08a674cdd5c8921519576ccf
franedsandi <franedsandi@gmail.com>
2023-11-22 16:19:34 +0100

    [feat] controller, home, db table,

---
+++ database/migrations/2023_11_22_150133_create_characters_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('height',5,2)->unsigned();
+            $table->decimal('weight',8,2)->unsigned();
+            $table->text('backgound');
+            $table->string('picture');
+            $table->tinyInteger('armor_class')->unsigned();
+            $table->tinyInteger('for')->unsigned();
+            $table->tinyInteger('des')->unsigned();
+            $table->tinyInteger('cos')->unsigned();
+            $table->tinyInteger('int')->unsigned();
+            $table->tinyInteger('sag')->unsigned();
+            $table->tinyInteger('car')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.main')
+
+@section('content')
+
+<h1>Ciao</h1>
+@endsection
