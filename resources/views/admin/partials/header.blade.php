18eb77618be2226d088419e806d7d8a57ac3233c
silviapetrino <petrino.silvia@gmail.com>
2024-01-07 17:32:28 +0100

    [fix] profile page, header style

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,6 +1,7 @@
-<header>
-    <div class="d-flex align-items-center justify-content-between px-5 " style="background-color: rgba(0,0,0, 0.8)">
-        <div class="d-flex align-items-center">
+<header >
+
+    <div class="d-flex justify-content-between align-items-center" style="background-color: rgba(0,0,0, 0.8)">
+        <div class="top d-flex align-items-center">
             <div class="logo p-1" style="width: 80px">
                 <img class="d-inline-block img-fluid w-100 h-100" src="/logoDandD.png" alt="logoDeD">
             </div>
@@ -11,15 +12,16 @@
         </div>
 
 
-        <ul class="nav nav-pills gap-3">
+        <ul class="nav nav-pills gap-2 " >
             <li class="nav-item"><a href="{{Route('admin.home')}}" class="nav-link active" aria-current="page">Home</a></li>
             <li class="nav-item"><a href="{{Route('admin.characters.index')}}" class="nav-link active" aria-current="page">Character List</a></li>
             <li class="nav-item">@include('admin.partials.create_boton', ['route' => route('admin.characters.create'), 'add' => 'Make your own character'] )</li>
             <li class="nav-item"><a href="{{Route('admin.races.index')}}" class="nav-link active" aria-current="page">Races list</a></li>
             <li class="nav-item"><a href="{{Route('admin.skills.index')}}" class="nav-link active" aria-current="page">Skills list</a></li>
         </ul>
-        <ul class="navbar-nav ml-auto ">
-            <div class="d-flex gap-3">
+
+        <ul class="bottom navbar-nav ml-auto">
+
                 @guest
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -30,7 +32,7 @@
                 </li>
                 @endif
                 @else
-                <li class="nav-item dropdown text-white">
+                <li class="nav-item dropdown text-white d-flex justify-content-end pe-4">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }}
                     </a>
@@ -51,7 +53,7 @@
                     </div>
                 </li>
                 @endguest
-            </div>
+
         </ul>
     </div>
 </header>
