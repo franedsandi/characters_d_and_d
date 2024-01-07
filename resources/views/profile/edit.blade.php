f037c75c0376d3c11cc88939a25e628d9ba01d2c
silviapetrino <petrino.silvia@gmail.com>
2024-01-07 18:41:43 +0100

    [edit] style

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="container p-5">
+<div class="container login p-5">
     <h2 class="fs-4 text-secondary text-white">
         {{ __('Profile') }}
     </h2>
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+    <div class="card p-4 mb-4  shadow rounded-lg">
 
         @include('profile.partials.update-profile-information-form')
 
     </div>
 
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+    <div class="card p-4 mb-4  shadow rounded-lg">
 
 
         @include('profile.partials.update-password-form')
 
     </div>
 
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+    <div class="card p-4 mb-4  shadow rounded-lg">
 
 
         @include('profile.partials.delete-user-form')
